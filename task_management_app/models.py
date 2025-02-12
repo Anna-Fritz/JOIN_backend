6d0d17a4501a0a86e9e4aeb136b925158f4a1d71
Anna Fritz <anna_eida@gmx.de>
2025-02-13 00:32:40 +0100

    added User Model

--- task_management_app/models.py
+++ task_management_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    contactNumber = models.IntegerField()
+    color = models.CharField(max_length=7)
+
