c3883a919ffb0676a59f1c86fd737b48c66f8aa9
Anna Fritz <anna_eida@gmx.de>
2025-02-13 01:16:40 +0100

    added Task Model & related Models for Tasks

--- task_management_app/models.py
+++ task_management_app/models.py
@@ -3,9 +3,45 @@ from django.db import models
 # Create your models here.
 
 
-class User(models.Model):
+class User (models.Model):
     username = models.CharField(max_length=30)
     email = models.CharField(max_length=30)
     contactNumber = models.IntegerField()
     color = models.CharField(max_length=7)
 
+
+class AssignedUser(models.Model):
+    username = models.CharField(max_length=30)
+    initials = models.CharField(max_length=2)
+    color = models.CharField(max_length=7)
+
+
+class Category(models.Model):
+    name = models.CharField()
+    color = models.CharField(max_length=7)
+
+
+class Prio(models.Model):
+    level = models.CharField()
+    icon_path = models.CharField()
+
+
+class Subtask(models.Model):
+    checkbox_img = models.CharField()
+    subtask = models.CharField()
+
+
+class SubtaskDone(models.Model):
+    subtask_done = models.CharField()
+
+
+class Task (models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    due_date = models.DateField()
+    status = models.CharField()
+    assigned_users = models.ManyToManyField(AssignedUser)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    prio = models.ForeignKey(Prio, on_delete=models.CASCADE)
+    subtasks = models.ManyToManyField(Subtask)
+    subtasks_done = models.ManyToManyField(SubtaskDone)
