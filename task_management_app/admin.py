9c7b80a8c1562e48f3952498c405072f4583e40f
Anna Fritz <anna_eida@gmx.de>
2025-02-13 14:20:40 +0100

    feat(admin): register models in Django admin panel

--- task_management_app/admin.py
+++ task_management_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, AssignedUser, Category, Prio, Subtask, SubtaskDone, Summary, Task
 
 # Register your models here.
+
+admin.site.register([User, AssignedUser, Category, Prio, Subtask, SubtaskDone, Summary, Task])
