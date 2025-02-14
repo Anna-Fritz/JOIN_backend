0933df1442ec53fc8516b1fcc58c40d37aeee20c
Anna Fritz <anna_eida@gmx.de>
2025-02-14 11:03:22 +0100

    feat(url): creat 'task' path

--- task_management_app/api/urls.py
+++ task_management_app/api/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import UsersView, UserSingleView
+from .views import UsersView, UserSingleView, TasksView
 
 urlpatterns = [
     path('user/', UsersView.as_view()),
-    path('user/<int:pk>/', UserSingleView.as_view())
+    path('user/<int:pk>/', UserSingleView.as_view()),
+    path('task/', TasksView.as_view())
+
 ]
