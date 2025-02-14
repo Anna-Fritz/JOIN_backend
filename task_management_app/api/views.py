469bb6faa48227091d3b45d0cb86d9fbc2c2b4f1
Anna Fritz <anna_eida@gmx.de>
2025-02-14 11:03:45 +0100

    feat(view): create TasksView

--- task_management_app/api/views.py
+++ task_management_app/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
-from ..models import User
-from .serializers import UserSerializer
+from ..models import User, Task
+from .serializers import UserSerializer, TaskSerializer
 
 
 class UsersView(generics.ListCreateAPIView):
@@ -13,3 +13,8 @@ class UsersView(generics.ListCreateAPIView):
 class UserSingleView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class TasksView(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
