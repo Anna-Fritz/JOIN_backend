c15fde615272dea9223995599b4532fbe8352921
Anna Fritz <anna_eida@gmx.de>
2025-02-14 11:02:38 +0100

    feat(serializer): create TaskSerializer & CategorySerializer

--- task_management_app/api/serializers.py
+++ task_management_app/api/serializers.py
@@ -7,3 +7,31 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+
+class CategorySerialzer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    assigned_users = UserSerializer(many=True, read_only=True)
+    assigned_user_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+        many=True,
+        write_only=True,
+        source='assigned_users'
+    )
+    category = CategorySerialzer(read_only=True)
+    category_id = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        write_only=True,
+        source='category'
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
