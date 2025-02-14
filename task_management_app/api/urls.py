7e5e68b929d816ed228fc3fec4152fa64a1704a6
Anna Fritz <anna_eida@gmx.de>
2025-02-14 12:38:11 +0100

    feat(view): create SubtasksView & SubtasksDoneView + add task/pk/subtask/ path

--- task_management_app/api/urls.py
+++ task_management_app/api/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import UsersView, UserSingleView, TasksView, TaskSingleView
+from .views import UsersView, UserSingleView, TasksView, TaskSingleView, SubtasksView, \
+    SubtasksDoneView
 
 urlpatterns = [
     path('user/', UsersView.as_view()),
     path('user/<int:pk>/', UserSingleView.as_view()),
     path('task/', TasksView.as_view()),
-    path('task/<int:pk>/', TaskSingleView.as_view())
-
+    path('task/<int:pk>/', TaskSingleView.as_view()),
+    path('task/<int:pk>/subtask/', SubtasksView.as_view()),
+    path('task/<int:pk>/subtask-done/', SubtasksDoneView.as_view())
 
 ]
--- task_management_app/api/views.py
+++ task_management_app/api/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework import generics
-from ..models import User, Task
-from .serializers import UserSerializer, TaskSerializer
+from ..models import User, Task, Subtask, SubtaskDone
+from .serializers import UserSerializer, TaskSerializer, SubtaskSerializer, SubtaskDoneSerializer
 
 
 class UsersView(generics.ListCreateAPIView):
@@ -23,3 +23,35 @@ class TasksView(generics.ListCreateAPIView):
 class TaskSingleView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+class SubtasksView(generics.ListCreateAPIView):
+    # queryset = Subtask.objects.all()
+    serializer_class = SubtaskSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        task = Task.objects.get(pk=pk)
+        return task.subtasks.all()
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        task = Task.objects.get(pk=pk)
+        subtask = serializer.save()
+        task.subtasks.add(subtask)
+
+
+class SubtasksDoneView(generics.ListCreateAPIView):
+    # queryset = SubtaskDone.objects.all()
+    serializer_class = SubtaskDoneSerializer
+
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+        task = Task.objects.get(pk=pk)
+        return task.subtasks_done.all()
+
+    def perform_create(self, serializer):
+        pk = self.kwargs.get('pk')
+        task = Task.objects.get(pk=pk)
+        subtask_done = serializer.save()
+        task.subtasks_done.add(subtask_done)
