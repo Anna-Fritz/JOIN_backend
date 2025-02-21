6b02f4f8d9ff60f4cfb0d69fb607d6df00fea125
Anna Fritz <anna_eida@gmx.de>
2025-02-21 22:08:36 +0100

    refactor(url): delete url paths for subtask-done

--- task_management_app/api/urls.py
+++ task_management_app/api/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import UsersView, UserSingleView, TasksView, TaskSingleView, SubtasksView, \
-    SubtasksDoneView, SubtaskSingleView, SubtaskDoneSingleView, SummaryView, PriosView, CategoriesView
+    SubtaskSingleView, SummaryView, PriosView, CategoriesView
 
 urlpatterns = [
     path('user/', UsersView.as_view()),
@@ -10,8 +10,6 @@ urlpatterns = [
     path('task/<int:pk>/', TaskSingleView.as_view()),
     path('task/<int:pk>/subtask/', SubtasksView.as_view()),
     path('task/<int:task_id>/subtask/<int:pk>/', SubtaskSingleView.as_view(), name='subtask-detail'),
-    path('task/<int:pk>/subtask-done/', SubtasksDoneView.as_view()),
-    path('task/<int:task_id>/subtask-done/<int:pk>/', SubtaskDoneSingleView.as_view(), name='subtask-done-detail'),
     path('summary/', SummaryView.as_view()),
     path('prio/', PriosView.as_view()),
     path('category/', CategoriesView.as_view())
