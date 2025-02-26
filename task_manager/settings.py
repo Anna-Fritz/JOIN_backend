397b546bc41d68c52ec3bf702085c561e11f602e
Anna Fritz <anna_eida@gmx.de>
2025-02-26 01:03:43 +0100

    feat(app): create user_auth_app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_management_app'
+    'task_management_app',
+    'user_auth_app'
 ]
 
 MIDDLEWARE = [
