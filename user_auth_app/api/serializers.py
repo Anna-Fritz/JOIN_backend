40540a7335b9b67098f4c8c34a7eadd96f9b40a4
Anna Fritz <anna_eida@gmx.de>
2025-02-27 23:44:09 +0100

    feat(serializer): create EmailAuthTokenSerializer

--- user_auth_app/api/serializers.py
+++ user_auth_app/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from ..models import UserProfile
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -26,13 +27,38 @@ class RegistrationSerializer(serializers.ModelSerializer):
         repeated_pw = data['repeated_password']
 
         if pw != repeated_pw:
-            raise serializers.ValidationError({'error': 'passwords do not match'})
+            raise serializers.ValidationError({'error': 'Passwords do not match'})
 
         if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError({'email': 'Email already exists'})
+            raise serializers.ValidationError({'error': 'Invalid email or password'})
 
         return data
 
     def create(self, validated_data):
         validated_data.pop('repeated_password')
         return User.objects.create_user(**validated_data)
+
+
+class EmailAuthTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, trim_whitespace=False)
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                raise serializers.ValidationError({'error': 'Invalid email or password'})
+
+            user = authenticate(username=user.username, password=password)
+
+            if not user:
+                raise serializers.ValidationError({'error': 'Invalid email or password'})
+        else:
+            raise serializers.ValidationError({'error': 'Must include "email" and "password"'})
+
+        attrs['user'] = user
+        return attrs
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from ..models import UserProfile
-from .serializers import UserProfileSerializer, RegistrationSerializer
+from .serializers import UserProfileSerializer, RegistrationSerializer, EmailAuthTokenSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
@@ -44,7 +44,7 @@ class LoginView(ObtainAuthToken):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        serializer = self.serializer_class(data=request.data)
+        serializer = EmailAuthTokenSerializer(data=request.data)
 
         if serializer.is_valid():
             try:
