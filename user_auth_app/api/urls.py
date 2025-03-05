4fb153ac57ae6fc38cf285cc3d61684615652067
Anna Fritz <anna_eida@gmx.de>
2025-03-05 01:52:14 +0100

    feat(url): create new paths for login, refresh and logout

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import UserProfileList, UserProfileDetail, RegistrationView, LoginView
+from .views import UserProfileList, UserProfileDetail, RegistrationView, LoginView, login_view, refresh_view, logout_view
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', login_view, name='login'),
+    path('login/refresh/', refresh_view, name='refresh'),
+    path('logout/', logout_view, name='logout'),
+    # path('login/', LoginView.as_view(), name='login')
 ]
