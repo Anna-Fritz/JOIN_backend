38736e1c7558128b1df6777934c5a875ab089b70
Anna Fritz <anna_eida@gmx.de>
2025-03-08 00:01:28 +0100

    feat(url): create paths for guest login und logout

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'task_management_app',
     'user_auth_app',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist'
 ]
 
 MIDDLEWARE = [
@@ -181,7 +182,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserProfileList, UserProfileDetail, RegistrationView, login_view, refresh_view, logout_view
+from .views import UserProfileList, UserProfileDetail, RegistrationView, login_view, refresh_view, logout_view, GuestLoginView, GuestLogoutView
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', login_view, name='login'),
     path('login/refresh/', refresh_view, name='refresh'),
+    path('login/guest/', GuestLoginView.as_view(), name="guest-login"),
     path('logout/', logout_view, name='logout'),
+    path('logout/guest/', GuestLogoutView.as_view(), name="guest-logout")
 ]
